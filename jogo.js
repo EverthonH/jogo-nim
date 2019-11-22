4401ce47b1c9ac0f9cd4cd6f2aac6e7b3ea03059
Everthon Henrique <55612895+EverthonH@users.noreply.github.com>
2019-11-22 03:45:45 -0300

    Segunda Entrega

--- jogo.js
+++ jogo.js
@@ -1,86 +1,112 @@
 var express = require ('express');
 var mustache = require ('mustache-express');
 var app = express ();
+var port = 8080;
 
 app.engine ('html', mustache ());
 app.set ('view engine', 'html');
 app.set ('views', __dirname + '/paginas');
 
-var tabela = [[0,0,0,0,1],
-			  [0,0,0,1,1],
-			  [0,0,1,1,1],
-			  [0,1,1,1,1],
-			  [1,1,1,1,1]
+function gel() {
+		return [[0,0,0,0,1],
+			  	[0,0,0,1,1],
+			  	[0,0,1,1,1],
+			  	[0,1,1,1,1],
+			  	[1,1,1,1,1]
 ];
-//link para o menu
+};
+var jogador = 0;
+
+var tabela = gel();
 app.get ('/', function (req, res) {
+	tabela = gel();
 	res.render ('menu.html', {'nome':'nim'});
 });
-
 app.get("/jogo", function(req, res){
+	var i = parseInt(req.query.i) || 0;
+	var j = parseInt(req.query.j) || 0;
+	/*console.log(i, j);*/
+	if(i >= 0 && j >= 0) {
+		jogador = (jogador + 1) % 2;
+		tabela[i][j] = 0;
+		
+		//logica do {
+		}
+		for(x=0;x<=i;x++){
+			tabela[x][j] = 0;			
+			}
+		
 	res.write('<!DOCTYPE html>')
     res.write('<html>');
     res.write('<html lang="pt-br">')
     res.write('<meta charset="UTF-8">')
     res.write('<title>jogar</title>')
-    res.write('<link rel="stylesheet" type="text/css" href="css/sobre.css">')
-    res.write('<link rel="stylesheet" type="text/css" href="css/comum.css">')
+    res.write('<link rel="stylesheet" type="text/css" href="css/jogar.css">')
     res.write('<link rel="stylesheet icon" href="icon.png">')
-    res.write('</head>')
+    res.write('</head>');
     res.write('<body>');
     res.write('<table>');
-
+    
+    soma = 0
 for(i = 0;i<tabela.length;i++){
 	 res.write('<tr>');
 	for(j = 0; j < tabela[i].length;j++){
 		if(tabela[i][j]==1){
-			res.write(`<td><img class="img" src="palito.png">${i},${j}</td>`);
+			res.write(`<td><a href="/jogo?i=${i}&j=${j}"><img class="img" src="palito.png"></a></td>`);
 		}
-
 		else if(tabela[i][j] ==0){
 			res.write('<td></td>');
 		}
+		soma += tabela[i][j];
 	}
 	res.write('</tr>');
 }
+
 	res.write('</table>');
 
+	if(soma == 0  && jogador == 0){
+		res.write(`<h1>Mais uma Ficha Porfavor, Jogador 1 Ganhou.</h1>`);
+	}
+	else if(soma == 0  && jogador == 1 ){
+		res.write(`<h1>Mais uma Ficha Porfavor, Jogador 2 Ganhou.</h1>`);
+	}
+  
+	else if(jogador == 1){
+		res.write('<p>Jogador 1</p>');
+	}
+	else{
+		res.write('<p>Jogador 2</p>');
+	}
+   
+	
 
-	
-	
-	res.write('<ul class="ul">')
-	res.write('<li class="li"><a class="a" href="/">Voltar</a></li>')
+
+	res.write('<ul>')
+	res.write('<li> <a href="/">inicio</a></li>')
 	res.write('</ul> ')
-
     res.write('</body>');
     res.write('</html>');
     res.end();
+
 });
 
-//link pra a pagina Sobre
 app.get("/sobre", function(req, res){
 	res.render(__dirname + "/paginas/sobre.html");
 });
-//link pra a pagina Contato
 app.get("/contato", function(req, res){
 	res.render(__dirname + "/paginas/contato.html");
 });
 
-//link pra a pagina ajuda
 app.get("/ajuda", function(req, res){
 	res.render(__dirname + "/paginas/ajuda.html");
 });
 
-//Link para o css
 app.use('/css', express.static(__dirname + '/css'));
 
-//Link para o imagens
-
 app.use(express.static('imagens'));
+
 app.use(express.static('css'));
 
-//porta que o servidor vai rodar
-var port = 3000;
 app.listen (port, function () {
 	console.log (`Escutando na porta ${port}...`);
 })
