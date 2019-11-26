f9bd432c6303239f94173bb5ed27cb92a485412d
Everthon Henrique <55612895+EverthonH@users.noreply.github.com>
2019-11-26 11:37:37 -0300

    Atualização do código

--- jogo.js
+++ jogo.js
@@ -30,19 +30,19 @@ app.get("/jogo", function(req, res){
 		jogador = (jogador + 1) % 2;
 		tabela[i][j] = 0;
 		
-		//logica do {
+		//logica do jogo {
 		}
 		for(x=0;x<=i;x++){
 			tabela[x][j] = 0;			
 			}
 		
-	res.write('<!DOCTYPE html>')
+	res.write('<!DOCTYPE html>');
     res.write('<html>');
-    res.write('<html lang="pt-br">')
-    res.write('<meta charset="UTF-8">')
-    res.write('<title>jogar</title>')
-    res.write('<link rel="stylesheet" type="text/css" href="css/jogar.css">')
-    res.write('<link rel="stylesheet icon" href="icon.png">')
+    res.write('<html lang="pt-br">');
+    res.write('<meta charset="UTF-8">');
+    res.write('<title>jogar</title>');
+    res.write('<link rel="stylesheet" type="text/css" href="css/jogar.css">');
+    res.write('<link rel="stylesheet icon" href="icon.png">');
     res.write('</head>');
     res.write('<body>');
     res.write('<table>');
@@ -51,10 +51,10 @@ app.get("/jogo", function(req, res){
 for(i = 0;i<tabela.length;i++){
 	 res.write('<tr>');
 	for(j = 0; j < tabela[i].length;j++){
-		if(tabela[i][j]==1){
+		if(tabela[i][j] == 1){
 			res.write(`<td><a href="/jogo?i=${i}&j=${j}"><img class="img" src="palito.png"></a></td>`);
 		}
-		else if(tabela[i][j] ==0){
+		else if(tabela[i][j] == 0){
 			res.write('<td></td>');
 		}
 		soma += tabela[i][j];
@@ -65,10 +65,10 @@ for(i = 0;i<tabela.length;i++){
 	res.write('</table>');
 
 	if(soma == 0  && jogador == 0){
-		res.write(`<h1>Mais uma Ficha Porfavor, Jogador 1 Ganhou.</h1>`);
+		res.write(`<h1>Mais uma Ficha Por favor, Jogador 1 Ganhou.</h1>`);
 	}
 	else if(soma == 0  && jogador == 1 ){
-		res.write(`<h1>Mais uma Ficha Porfavor, Jogador 2 Ganhou.</h1>`);
+		res.write(`<h1>Mais uma Ficha Por favor, Jogador 2 Ganhou.</h1>`);
 	}
   
 	else if(jogador == 1){
@@ -81,9 +81,9 @@ for(i = 0;i<tabela.length;i++){
 	
 
 
-	res.write('<ul>')
-	res.write('<li> <a href="/">inicio</a></li>')
-	res.write('</ul> ')
+	res.write('<ul>');
+	res.write('<li> <a href="/">inicio</a></li>');
+	res.write('</ul> ');
     res.write('</body>');
     res.write('</html>');
     res.end();
@@ -91,112 +91,112 @@ for(i = 0;i<tabela.length;i++){
 });
 
 app.get("/sobre", function(req, res){
-	res.write('<!DOCTYPE html>')
-res.write('<html lang="br">')
-res.write('<head>')
-	res.write('<meta charset="UTF-8">')
+	res.write('<!DOCTYPE html>');
+	res.write('<html lang="br">');
+	res.write('<head>');
+		res.write('<meta charset="UTF-8">');
 
-	res.write('<title>Sobre o Nim</title>')
-	res.write('<!-- Link para o css -->') 
-	res.write('<link rel="stylesheet" type="text/css" href="css/sobre.css">')
-	res.write ('<link rel="stylesheet icon" href="icon.png">')
-res.write('</head>')
-res.write('<body>')
-	res.write('<!-- Contatos -->')
-	res.write('<div class="container">')
-		res.write('<div class="box">')
-			res.write('<div class="imgbx">')
-				res.write('<img src="img1.jpg">')
-			res.write('</div>')
-			res.write('<div class="content">')
-				res.write('<div>')
-					res.write('<h2>Lucas R</h2>')
-					res.write('<p>Meu nome é Lucas Robson, um dos desenvolvedores do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail: lrmds@discente.ifpe.edu.brres.write</p>')
-				res.write('</div>')
-			res.write('</div>')
-		res.write('</div>')
-		res.write('<div class="box">')
-			res.write('<div class="imgbx">')
-				res.write('<img src="img3.jpg">')
-			res.write('</div>')
-			res.write('<div class="content">')
-				res.write('<div>')
-					res.write('<h2>Everthon H</h2>')
-					res.write('<p>Meu nome é Everthon Henrique, um dos desenvolvedores do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail: ehpb@discente.ifpe.edu.br</p>')
-				res.write('</div>')
-			res.write('</div>')
-		res.write('</div> ')
+		res.write('<title>Sobre o Nim</title>');
+		res.write('<!-- Link para o css -->') ;
+		res.write('<link rel="stylesheet" type="text/css" href="css/sobre.css">');
+		res.write ('<link rel="stylesheet icon" href="icon.png">');
+	res.write('</head>');
+	res.write('<body>');
+			res.write('<!-- Contatos -->');
+		res.write('<div class="container">');
+		res.write('<div class="box">');
+		res.write('<div class="imgbx">');
+		res.write('<img src="img1.jpg">');
+			res.write('</div>');
+			res.write('<div class="content">');
+				res.write('<div>');
+					res.write('<h2>Lucas R</h2>');
+					res.write('<p>Meu nome é Lucas Robson, um dos desenvolvedores do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail: lrmds@discente.ifpe.edu.br</p>');
+				res.write('</div>');
+			res.write('</div>');
+		res.write('</div>');
+		res.write('<div class="box">');
+			res.write('<div class="imgbx">');
+				res.write('<img src="img3.jpg">');
+			res.write('</div>');
+			res.write('<div class="content">');
+				res.write('<div>');
+					res.write('<h2>Everthon H</h2>');
+					res.write('<p>Meu nome é Everthon Henrique, um dos desenvolvedores do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail: ehpb@discente.ifpe.edu.br</p>');
+				res.write('</div>');
+			res.write('</div>');
+		res.write('</div> ');
 
-		res.write('<div class="box">')
-			res.write('<div class="imgbx">')
-				res.write('<img src="capture.png">')
-			res.write('</div>')
-			res.write('<div class="content">')
-				res.write('<div>')
-					res.write('<h2>Guilherme V</h2>')
-					res.write('<p>Meu nome é Guilherme Valença, um dos desenvolvedores do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail gvrp@discente.ifpe.edu.br</p>')
-				res.write('</div>')
-			res.write('</div>')
-		res.write('</div>')
-		res.write('<div class="box">')
-			res.write('<div class="imgbx">')
-				res.write('<img src="img4.png">')
-			res.write('</div>')
-			res.write(('<div class="content">'))
-				res.write('<div>')
-					res.write('<h2>Allan L</h2>')
-					res.write('<p>Meu nome é Allan Lima, o Professor Responsavel pelo projeto do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail: allanlima@igrassu.ifpe.edu.br</p>')
-				res.write('</div>')
-			res.write('</div>')
-		res.write('</div>')
-	res.write('<div class="go">')
+		res.write('<div class="box">');
+			res.write('<div class="imgbx">');
+				res.write('<img src="capture.png">');
+			res.write('</div>');
+			res.write('<div class="content">');
+				res.write('<div>');
+					res.write('<h2>Guilherme V</h2>');
+					res.write('<p>Meu nome é Guilherme Valença, um dos desenvolvedores do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail gvrp@discente.ifpe.edu.br</p>');
+				res.write('</div>');
+			res.write('</div>');
+		res.write('</div>');
+		res.write('<div class="box">');
+			res.write('<div class="imgbx">');
+				res.write('<img src="img4.png">');
+			res.write('</div>');
+			res.write('<div class="content">');
+				res.write('<div>');
+					res.write('<h2>Allan L</h2>');
+					res.write('<p>Meu nome é Allan Lima, o Professor Responsavel pelo projeto do jogo NIM</br>qualquer duvida pode entrar em contato com o E-mail: allanlima@igrassu.ifpe.edu.br</p>');
+				res.write('</div>');
+			res.write('</div>');
+		res.write('</div>');
+	res.write('<div class="go">');
 	 res.write ('<p class="pp">Verção 0.2--22/11/2019.</br>IFPE -Instututo Federal de Edurção Ciencia e Tecnologia de Pernambuco </br>Lógica de programação.</br>')
-	res.write('</p>')
-	res.write('<img class="img-sobre" src="logo.png">')
-	res.write('<ul style="position: absolute; left: 0; bottom: 20px;">')
-	res.write('<li><a href="/">Voltar</a></li>')
-	res.write('</ul>')
+	res.write('</p>');
+	res.write('<img class="img-sobre" src="logo.png">');
+	res.write('<ul style="position: absolute; left: 0; bottom: 20px;">');
+	res.write('<li><a href="/">Voltar</a></li>');
+	res.write('</ul>');
 
 	
-res.write('</body>')
-res.write('</html>')
+res.write('</body>');
+res.write('</html>');
 
 });
 app.get("/ajuda", function(req, res){
 	
-res.write('<!DOCTYPE html>')
-res.write('<html lang="br">')
-res.write('<head>')
-res.write('<meta charset="UTF-8">')
-res.write('<title>Ajuda</title>')
+res.write('<!DOCTYPE html>');
+res.write('<html lang="br">');
+res.write('<head>');
+res.write('<meta charset="UTF-8">');
+res.write('<title>Ajuda</title>');
 	
 		<!-- Link pra o css --> 
-res.write('<link rel="stylesheet" type="text/css" href="css/ajuda.css">')
-res.write('<link rel="stylesheet icon" href="icon.png">')
-res.write('</head>')
-res.write('<body>')
+res.write('<link rel="stylesheet" type="text/css" href="css/ajuda.css">');
+res.write('<link rel="stylesheet icon" href="icon.png">');
+res.write('</head>');
+res.write('<body>');
 
-res.write('<h1 class="tex">Instruções do Jogo</h1>')
+res.write('<h1 class="tex">Instruções do Jogo</h1>');
 
-res.write('<p >Cada jogada consiste em retirar algumas (ou todas) as peças de um dos montes.<br />Os números de peças e de montes podem variar, sendo a distribuição mais comum<br /> constituida por 15 peças em montes de 1,2,3,4 e 5 peças.<br />')
-res.write('</p>')
-res.write('<h2 class="tex">ORIGEM DO NIM</h2>')
+res.write('<p >Cada jogada consiste em retirar algumas (ou todas) as peças de um dos montes.<br />Os números de peças e de montes podem variar, sendo a distribuição mais comum<br /> constituida por 15 peças em montes de 1,2,3,4 e 5 peças.<br />');
+res.write('</p>');
+res.write('<h2 class="tex">ORIGEM DO NIM</h2>');
 	
 res.write('<p>Nim é um jogo que foi criando na antiga China para dois jogadores. Foi o primeiro jogo a ser estudado pela Matemática.</p>')
 
-res.write('<h2 class="tex" >HISTORIA</h2>')
-res.write('<p>A origem do NIM é desconhecida, sendo jogado desde a antiguidade. O nome foi dado por um Matematico Americano chamado Charles L.<br /> Bouton em um artigo de 1902, onde estuda a teoria matemática do jogo.</br> O jogo ganhou notoriedade com a aparição no filme "O ano passado em Marienbad" de Alain Resnais, em 1961.</p>')
+res.write('<h2 class="tex" >HISTORIA</h2>');
+res.write('<p>A origem do NIM é desconhecida, sendo jogado desde a antiguidade. O nome foi dado por um Matematico Americano chamado Charles L.<br /> Bouton em um artigo de 1902, onde estuda a teoria matemática do jogo.</br> O jogo ganhou notoriedade com a aparição no filme "O ano passado em Marienbad" de Alain Resnais, em 1961.</p>');
 
 	
 
 		
-res.write('<ul>')
-res.write('<li>')
-res.write('<a href="/">Voltar</a>')
-res.write('</li>')
-res.write('</ul>')
-res.write('</body>')
-res.write('</html>')
+res.write('<ul>');
+res.write('<li>');
+res.write('<a href="/">Voltar</a>');
+res.write('</li>');
+res.write('</ul>');
+res.write('</body>');
+res.write('</html>');
 
 res.write('<!-- Wallpaper do jogo https://wallpaperscraft.com/catalog/space/3840x2160 -->')
 });
